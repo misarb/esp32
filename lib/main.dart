c7797d94af363f0982eca693a2dccc0dca87abfd
misarb <lahssane12@gmail.com>
2022-07-12 12:33:17 +0100

    UI Add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_appe/screens/iotscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,58 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FIOT Project',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: const MyHomePage(title: 'ESP32 Firebase IOT'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      routes: <String, WidgetBuilder>{
+        '/iotscreen': (BuildContext context) => IotScreen(),
+      },
+      debugShowCheckedModeBanner: true,
+      home: IotScreen(),
     );
   }
 }
---
+++ lib/screens/iotscreen.dart
@@ -0,0 +1,97 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class IotScreen extends StatefulWidget {
+  @override
+  State<IotScreen> createState() => _IotScreenState();
+}
+
+class _IotScreenState extends State<IotScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const Icon(
+                  Icons.clear_all,
+                ),
+                const Text(
+                  "Configurations",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Icon(Icons.settings)
+              ],
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Padding(
+                      padding: EdgeInsets.all(18),
+                      child: Text(
+                        "Temperateur",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                  Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "20.0",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                  SizedBox(height: 20.0),
+                  Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Motor Vitess",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                  Padding(
+                      padding: EdgeInsets.all(18),
+                      child: Text(
+                        "120",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  FloatingActionButton.extended(
+                    onPressed: () {},
+                    label: Text("ON"),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
